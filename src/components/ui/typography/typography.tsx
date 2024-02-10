1eb360e8277cf5effc64b228d6013e9f90afd93c
BorisKutsenko <boris.k@theevenstar.net>
2024-02-10 12:50:42 +0200

    set Plus Jakarta Sans font family as default

--- src/components/ui/typography/typography.tsx
+++ src/components/ui/typography/typography.tsx
@@ -1,4 +1,10 @@
+import { cva } from 'class-variance-authority';
 import React from 'react';
+
+const typographyVariants = cva('', {
+	variants: {},
+	defaultVariants: {},
+});
 
 export const Typography = () => {
 	return <div></div>;
