819a06f71e993cb955f39131b3e694bb49284734
BorisKutsenko <boris.k@theevenstar.net>
2024-02-10 13:36:31 +0200

    create Button component

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,5 +1,55 @@
-import React from 'react';
+import { cn } from '@utils/cn.ts';
+import { cva, VariantProps } from 'class-variance-authority';
+import React, { ReactNode } from 'react';
 
-export const Button = () => {
-	return <div></div>;
+const buttonVariants = cva('text-white font-bold transition duration-200', {
+	variants: {
+		size: {
+			s: 'rounded-[20px] py-2 px-16 text-[13px] leading-[23px]',
+			l: 'rounded-[24px] py-4 px-15 text-[15px] leading-[19px]',
+		},
+		color: {
+			primary: 'bg-purple hover:bg-purple-light',
+			secondary: 'bg-[#635FC729]  text-purple hover:bg-[#635FC740]',
+			destructive: 'bg-red hover:bg-red-light',
+		},
+	},
+	defaultVariants: {
+		size: 's',
+		color: 'primary',
+	},
+});
+
+type ButtonProps = {
+	children: ReactNode;
+	isLoading?: boolean;
+	disabled?: boolean;
+	onClick?: () => void;
+	className?: string;
+} & VariantProps<typeof buttonVariants>;
+
+export const Button = ({
+	children,
+	color,
+	className,
+	disabled,
+	isLoading,
+	size,
+	onClick,
+}: ButtonProps) => {
+	return (
+		<button
+			onClick={onClick}
+			disabled={disabled || isLoading}
+			className={cn(
+				buttonVariants({
+					color,
+					size,
+					className,
+				})
+			)}
+		>
+			{children}
+		</button>
+	);
 };
--- src/layouts/main-layout/main-layout.tsx
+++ src/layouts/main-layout/main-layout.tsx
@@ -1,13 +1,21 @@
+import { Button } from '@components/ui/button';
 import { Typography } from '@components/ui/typography';
 import React from 'react';
 
 export const MainLayout = () => {
 	return (
 		<>
-			<div className={'text-red-light'}>MainLayout</div>
+			<div
+				className={'bg-purple bg-opacity-[16%] text-sm font-bold text-red-light'}
+			>
+				MainLayout
+			</div>
 			<Typography component={'h2'} variant={'body-m'}>
 				hello
 			</Typography>
+			<Button>hello</Button>
+			<Button color={'destructive'}>hello</Button>
+			<Button color={'secondary'}>hello</Button>
 		</>
 	);
 };
