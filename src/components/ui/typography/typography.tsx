14f54709f6fb9af44800b3ccacc98734eb72bc3d
BorisKutsenko <boris.k@theevenstar.net>
2024-02-10 13:07:56 +0200

    create Typography component

--- src/components/ui/typography/typography.tsx
+++ src/components/ui/typography/typography.tsx
@@ -1,11 +1,40 @@
-import { cva } from 'class-variance-authority';
-import React from 'react';
+import { cn } from '@utils/cn.ts';
+import { cva, VariantProps } from 'class-variance-authority';
+import { createElement, ReactNode } from 'react';
 
-const typographyVariants = cva('', {
-	variants: {},
-	defaultVariants: {},
+const typographyVariants = cva('text-dark-gray font-bold', {
+	variants: {
+		variant: {
+			'body-m': 'text-[12px] leading-[15px]',
+			'body-l': 'font-medium text-[13px] leading-[23px]',
+			'heading-s': 'text-[12px] leading-[25px]',
+			'heading-m': 'text-[15px] leading-[19px]',
+			'heading-l': 'text-[18px] leading-[23px]',
+			'heading-xl': 'text-[24px] leading-[30px]',
+		},
+	},
+	defaultVariants: {
+		variant: 'heading-m',
+	},
 });
 
-export const Typography = () => {
-	return <div></div>;
+type TypographyProps = {
+	component?: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+	children: ReactNode;
+	className?: string;
+} & VariantProps<typeof typographyVariants>;
+
+export const Typography = ({
+	component = 'p',
+	children,
+	variant,
+	className,
+}: TypographyProps) => {
+	return createElement(
+		component,
+		{
+			className: cn(typographyVariants({ variant, className })),
+		},
+		children
+	);
 };
--- src/layouts/main-layout/main-layout.tsx
+++ src/layouts/main-layout/main-layout.tsx
@@ -1,5 +1,13 @@
+import { Typography } from '@components/ui/typography';
 import React from 'react';
 
 export const MainLayout = () => {
-	return <div className={'text-red-light'}>MainLayout</div>;
+	return (
+		<>
+			<div className={'text-red-light'}>MainLayout</div>
+			<Typography component={'h2'} variant={'body-m'}>
+				hello
+			</Typography>
+		</>
+	);
 };
