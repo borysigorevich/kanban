093ec852c0a99b2fc3cbd3f0611adf350d622286
borysigorevich <borysigorevich@gmail.com>
2024-02-11 18:57:48 +0200

    start adding dnd

--- src/components/pages/board/components/columns/columns.tsx
+++ src/components/pages/board/components/columns/columns.tsx
@@ -1,9 +1,10 @@
 import { ColumnItem } from '@components/pages/board/components/columns/components/column-item';
 import { CreateColumn } from '@components/pages/board/components/columns/components/create-column';
+import { StrictModeDroppable } from '@components/shared/strict-mode-droppable';
 import { useGetParams } from '@hooks/useGetParams.ts';
 import { useQueryGetBoard } from '@pages/board/hooks/useQueryGetBoard.ts';
-import { cn } from '@utils/cn.ts';
 import React from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
 import { Task } from '../../../../../generated/graphql.tsx';
 
 export const Columns = () => {
@@ -18,18 +19,28 @@ export const Columns = () => {
 	return (
 		<div className={'overflow-hidden'}>
 			{hasColumns ? (
-				<div className={cn('flex h-full gap-6 overflow-x-auto p-6')}>
-					{columns.map((column) => (
-						<ColumnItem
-							key={column!.id}
-							id={column!.id}
-							title={column!.title}
-							tasks={column!.Tasks as Task[]}
-							statuses={statuses}
-						/>
-					))}
-					<CreateColumn isColumn />
-				</div>
+				<DragDropContext onDragEnd={() => {}}>
+					<StrictModeDroppable droppableId="droppable-list">
+						{(provided) => (
+							<div
+								ref={provided.innerRef}
+								{...provided.droppableProps}
+								className={'flex h-full gap-6 overflow-x-auto p-6'}
+							>
+								{columns.map((column) => (
+									<ColumnItem
+										key={column!.id}
+										id={column!.id}
+										title={column!.title}
+										tasks={column!.Tasks as Task[]}
+										statuses={statuses}
+									/>
+								))}
+								<CreateColumn isColumn />
+							</div>
+						)}
+					</StrictModeDroppable>
+				</DragDropContext>
 			) : (
 				<CreateColumn />
 			)}
---
+++ src/components/shared/strict-mode-droppable/strict-mode-droppable.tsx
@@ -0,0 +1,21 @@
+import React, { FC, useEffect, useState } from 'react';
+import { Droppable, DroppableProps } from 'react-beautiful-dnd';
+
+export const StrictModeDroppable = ({ children, ...rest }: DroppableProps) => {
+	const [enabled, setEnabled] = useState(import.meta.env.MODE === 'production');
+
+	useEffect(() => {
+		const animation = requestAnimationFrame(() => setEnabled(true));
+
+		return () => {
+			cancelAnimationFrame(animation);
+			setEnabled(false);
+		};
+	}, []);
+
+	if (!enabled) {
+		return null;
+	}
+
+	return <Droppable {...rest}>{children}</Droppable>;
+};
